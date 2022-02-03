0230fda9d0f1dfabe4b06ab9884796bf45ea795e
Digital-Hurricane-of-Questions <dhq128@gmail.com>
2022-02-03 22:50:06 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from http import client
 import discord, token_name
 bot_client = discord.Client()
+print()
+#bot_user = discord.User(name="AWEDoo", id=4410)
+
+#print(bot_user.avatar_url_as())
+
 bot_client.run(token_name.token_name)
